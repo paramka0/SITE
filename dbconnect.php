9abcb2fd8ae6a0554a3c1a1e6370d930cd18105b
paramka0 <74873667+paramka0@users.noreply.github.com>
2024-05-10 03:39:39 +0700

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $mysqli = new mysqli('localhost','root','root','travel');
-    if ($mysqli->connect_error) {
-        die(' Connect Error ('.$mysqli->connect_errno.') '. $mysqli->connect_error);
-    }
-?>
